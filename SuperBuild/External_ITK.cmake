e1bdd285fe221a003e0138b8328e7899d5d09ffe
Alexy Pellegrini <alexy.pellegrini@kitware.com>
2026-01-19 14:45:21 +0000

    Fix Linux build

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -96,11 +96,6 @@ if(NOT DEFINED ITK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
     endforeach()
   endif()
 
-  set(build_shared_itk TRUE)
-  if(SLICERLIB_PYTHON_BUILD) # when building slicerlib wheel use a static ITK
-    set(build_shared_itk FALSE)
-  endif()
-
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${Slicer_${proj}_GIT_REPOSITORY}"
@@ -132,7 +127,7 @@ if(NOT DEFINED ITK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
       -DModule_SimpleITKFilters:BOOL=${Slicer_USE_SimpleITK}
       -DModule_GenericLabelInterpolator:BOOL=ON
       -DModule_AdaptiveDenoising:BOOL=ON
-      -DBUILD_SHARED_LIBS:BOOL=${build_shared_itk}
+      -DBUILD_SHARED_LIBS:BOOL=TRUE
       -DITK_INSTALL_NO_DEVELOPMENT:BOOL=ON
       -DKWSYS_USE_MD5:BOOL=ON # Required by SlicerExecutionModel
       -DITK_WRAPPING:BOOL=OFF #${BUILD_SHARED_LIBS} ## HACK:  QUICK CHANGE
