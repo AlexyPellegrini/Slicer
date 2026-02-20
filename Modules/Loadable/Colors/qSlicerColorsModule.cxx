dd075018a656089770add31e3952cf3c75e9b04f
Alexy Pellegrini <alexy.pellegrini@kitware.com>
2026-02-20 12:00:50 +0100

    ENH: Revert unneeded change to qSlicerColorsModule.cxx

--- Modules/Loadable/Colors/qSlicerColorsModule.cxx
+++ Modules/Loadable/Colors/qSlicerColorsModule.cxx
@@ -48,6 +48,10 @@
 #include "qSlicerSubjectHierarchyPluginHandler.h"
 #include "qSlicerSubjectHierarchyColorLegendPlugin.h"
 
+// DisplayableManager initialization
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkSlicerColorsModuleMRMLDisplayableManager)
+
 //-----------------------------------------------------------------------------
 class qSlicerColorsModulePrivate
 {
