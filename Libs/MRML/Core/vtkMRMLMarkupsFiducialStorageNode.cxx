9e8afa672fc2fd9a6871a12c0e1d744345816c44
Alexy Pellegrini <alexy.pellegrini@kitware.com>
2026-02-23 10:28:01 +0100

    ENH: Slicer Core wheel build support
    This commit contains the minimal changes required to build Slicer Core
    wheel with the lesser amount of changes within the Slicer codebase.

--- Libs/MRML/Core/vtkMRMLMarkupsFiducialStorageNode.cxx
+++ Libs/MRML/Core/vtkMRMLMarkupsFiducialStorageNode.cxx
@@ -21,7 +21,6 @@
 #include "vtkMRMLI18N.h"
 #include "vtkMRMLScene.h"
 #include "vtkMRMLMessageCollection.h"
-#include "vtkSlicerVersionConfigureMinimal.h"
 
 #include "vtkObjectFactory.h"
 #include "vtkStringArray.h"
@@ -755,7 +754,6 @@ int vtkMRMLMarkupsFiducialStorageNode::WriteDataInternal(vtkMRMLNode* refNode)
   }
 
   // put down a header
-  of << "# Markups fiducial file version = " << Slicer_VERSION << std::endl;
   of << "# CoordinateSystem = " << vtkMRMLMarkupsStorageNode::GetCoordinateSystemAsString(this->GetCoordinateSystem()) << std::endl;
 
   // label the columns
